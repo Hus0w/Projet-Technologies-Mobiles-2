69c880486f33e32d0b538d4b9c0adaaa04568453
hus0w <la226628@student.helha.be>
2024-12-23 19:58:48 +0100

    Initialisation du backend avec la création d'un projet Spring Boot ainsi qu'une base de données en MariaDB avec Docker

---
+++ tm2_backend/src/main/java/org/helha/be/tm2_backend/Tm2BackendApplication.java
@@ -0,0 +1,13 @@
+package org.helha.be.tm2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tm2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tm2BackendApplication.class, args);
+    }
+
+}
---
+++ tm2_backend/src/test/java/org/helha/be/tm2_backend/Tm2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.helha.be.tm2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tm2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
